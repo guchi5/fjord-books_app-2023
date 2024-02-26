5187b81062dc098006567161ea7f13f791de1c7e
guchi5 <zero19390901@gmail.com>
2024-02-26 21:59:52 +0900

    ユーザー認証の修正 - 認証画面の日本語化 - ログイン時のリダイレクト先を`/books`に設定 - ログアウト時のリダイレクト先を`accounts/sign_in`に設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_out_path_for(resource_or_scope) # rubocop:disable Lint/UnusedMethodArgument
+    '/accounts/sign_in'
+  end
+
+  def after_sign_in_path_for(resource_or_scope) # rubocop:disable Lint/UnusedMethodArgument
+    '/books'
+  end
 end
